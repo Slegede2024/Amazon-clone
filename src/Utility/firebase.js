da4195993bc76585424435f6a1e2e59077622677
Slegede2024 <surafel.kemisso@gmail.com>
2025-03-15 12:46:46 -0700

    "Update environment variables and Firebase config"

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,23 +1,19 @@
-/* eslint-disable no-undef */
+
 import firebase  from "firebase/compat/app";
 
 import { getAuth } from "firebase/auth";
 import  "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-// Your web app's Firebase configuration
-import dotenv from "dotenv";
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 export default firebaseConfig;
 
 
