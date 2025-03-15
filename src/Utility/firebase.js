76305ca5ef11662f6e3efe2838effd3342f8b4ad
Slegede2024 <surafel.kemisso@gmail.com>
2025-03-15 12:24:24 -0700

    Removed functions/.env from Git tracking

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import firebase  from "firebase/compat/app";
 
 import { getAuth } from "firebase/auth";
@@ -5,14 +6,20 @@ import  "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
+import dotenv from "dotenv";
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCY9O5oyUzEqrotZMXBLQqnCSPDQClHjE8",
-  authDomain: "clone-ef248.firebaseapp.com",
-  projectId: "clone-ef248",
-  storageBucket: "clone-ef248.firebasestorage.app",
-  messagingSenderId: "449369468724",
-  appId: "1:449369468724:web:4e0fdccda4d0162b927f61",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+export default firebaseConfig;
+
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
