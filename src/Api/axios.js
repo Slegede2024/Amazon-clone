b8058f417f0143bf6a1787b9910437da687545db
Slegede2024 <surafel.kemisso@gmail.com>
2025-03-20 12:35:15 -0700

    Deploying latest updates

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  // baseURL: "http://127.0.0.1:5001/clone-ef248/us-central1/api",
-  baseURL: "https://api-wojinmq43q-uc.a.run.app",
+  baseURL: "http://127.0.0.1:5001/clone-ef248/us-central1/api",
 });
 
 export {axiosInstance}
